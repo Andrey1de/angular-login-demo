93816965c1a625c11b662e99b507e04757a88524
andre <andrey1de@gmail.com>
2019-03-21 01:22:12 +0200

    my commit

---
+++ create-git.bat
@@ -0,0 +1,6 @@
+echo "# angular-login-demo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Andrey1de/angular-login-demo.git
+git push -u origin master
